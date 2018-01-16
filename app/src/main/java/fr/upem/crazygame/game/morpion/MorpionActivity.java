d2c490da73aa88d75c48bafbef519d7f1f86d6b5
Android Project <zecchini.aymeric@gmail.com>
2018-01-16 12:18:29 +0100

    Update MorpionAcitivity
    Mise à jour suite à un problème de merging

---
+++ app/src/main/java/fr/upem/crazygame/game/morpion/MorpionActivity.java
@@ -0,0 +1,130 @@
+ package fr.upem.crazygame.game.morpion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.nio.channels.SocketChannel;
+
+import fr.upem.crazygame.R;
+import fr.upem.crazygame.morpion.Players;
+
+public class MorpionActivity extends AppCompatActivity {
+    private final static int NUMBER_CELL = 3;
+
+    private HandlerMorpion handlerMorpion;
+    private SocketChannel sc;
+    private Button[][] cases = new Button[NUMBER_CELL][NUMBER_CELL];
+
+    private TextView playerLeft;
+    private TextView playerRight;
+    private TextView messageBottom;
+
+    private boolean isTurn = false; //It's the turn of current player
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_morpion);
+        getInformation();
+    }
+
+    /**
+     * Init and launch the game
+     */
+    public void getInformation() {
+        Intent intent = getIntent();
+        Players begin = (Players) intent.getSerializableExtra("playerBegin");
+        Players current = (Players) intent.getSerializableExtra("playerCurrent");
+
+        //Init UI
+        initButton();
+
+        playerLeft = findViewById(R.id.joueur1);
+        playerRight = findViewById(R.id.joueur2);
+        messageBottom = findViewById(R.id.turn);
+
+        isTurn = begin.equals(current);
+
+        handlerMorpion = new HandlerMorpion (sc, this, new Morpion(begin, current));
+    }
+
+    /**
+     * Return the player who must play choose by the server
+     * When the player click on a cell of gameboard
+     */
+    public void clickCell (View view) {
+        if (isTurn) {
+            Button b = (Button) view;
+
+            for (int i = 0; i < MorpionActivity.NUMBER_CELL; i++) {
+                for (int j = 0; j < MorpionActivity.NUMBER_CELL; j++) {
+                    if (b.equals(cases[i][j])) {
+                        this.handlerMorpion.playAround(i, j);
+                        isTurn = false;
+                        waitOtherPlayer();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return
+     * Wait other player
+     */
+    public void waitOtherPlayer () {
+        if (!isTurn) {
+            this.handlerMorpion.waitOther();
+        }
+    }
+
+    public void isYourTurn () {
+        isTurn = true;
+    }
+
+    /**
+     * Init the gameBoard
+     */
+    public void initButton () {
+        cases[0][0] = ((Button)findViewById(R.id.button0));
+        cases[0][1] = ((Button)findViewById(R.id.button1));
+
+
+        cases[1][0] = ((Button)findViewById(R.id.button3));
+        cases[1][1] = ((Button)findViewById(R.id.button4));
+        cases[1][2] = ((Button)findViewById(R.id.button5));
+
+        cases[2][0] = ((Button)findViewById(R.id.button6));
+        cases[2][1] = ((Button)findViewById(R.id.button7));
+        cases[2][2] = ((Button)findViewById(R.id.button8));
+    }
+
+    public Button[][] getCases() {
+        return cases;
+    }
+
+    public Players getPlayer() {
+        return player;
+    }
+
+    public SocketChannel getSc() {
+        return sc;
+    }
+
+    public TextView getPlayerLeft() {
+        return playerLeft;
+    }
+
+    public TextView getPlayerRight() {
+        return playerRight;
+    }
+
+    public TextView getMessageBottom() {
+        return messageBottom;
+    }
+}
